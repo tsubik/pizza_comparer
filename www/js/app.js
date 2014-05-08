c8a5bf813f7fefd36b9edf7956d383fbefce8f79
tsubik <tsubik@gmail.com>
2014-05-08 21:05:57 +0200

    adding new pizzas

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,37 @@
 angular.module('pizza_comparer', ['ionic'])
 
-.controller('PizzaController', function($scope){
+.controller('PizzaController', function($scope, $ionicModal){
+
+    $ionicModal.fromTemplateUrl('pizza-modal.html',{
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function(modal){
+        $scope.pizzaModal = modal;
+    });
+
     $scope.pizzas = [
         { name: 'mała pizza', size: 30, price: 16.50 },
         { name: 'średnia pizza', size: 42, price: 18.99 },
         { name: 'duża pizza', size: 50, price: 21.20 }  
     ];
+
+    $scope.newPizza = function(){
+        $scope.pizzaModal.show();
+    };
+
+    $scope.createPizza = function(pizza){
+        $scope.pizzas.push({
+            name: pizza.name,
+            size: pizza.size,
+            price: pizza.price
+        });
+        $scope.pizzaModal.hide();
+        pizza.name = "";
+        pizza.size = "";
+        pizza.price = "";
+    };
+
+    $scope.closeNewPizza = function(){
+        $scope.pizzaModal.hide();
+    };
 });
